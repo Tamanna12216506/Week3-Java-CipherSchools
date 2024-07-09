d1f772583dad88f84dbd77df88f2c19d2aa00fdf
Tamanna <119649576+Tamanna12216506@users.noreply.github.com>
2024-07-09 21:27:21 +0530

    Create Lecture16-MultiDimArrays.java

---
+++ Lecture16-MultiDimArrays.java
@@ -0,0 +1,27 @@
+package arrays;
+public class MultiDimArrays{
+  public static void main(String[] args){
+    int rows = 5;
+    int cols = 3;
+    int arr[][] = new int[rows][cols];  //Array creation Expression
+  for(int i = 0;i<arr.length;i++){
+    for(int j = 0;j<arr[0].length;j++)   ///i->rows   j-> columns
+      {
+        System.out.print(arr[i][j]+" ");
+      }
+    System.out.println():
+      }
+    int arr2[][] = {
+      {1,2,3},
+      {4,5,6},
+      {7,8,9}
+    };
+     for(int i = 0;i<arr2.length;i++){
+    for(int j = 0;j<arr2[0].length;j++)   ///i->rows   j-> columns
+      {
+        System.out.print(arr2[i][j]+" ");
+      }
+    System.out.println():
+      }
+  }
+}
